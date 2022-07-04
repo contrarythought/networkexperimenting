1a4bb46c60cf989c41cad159d263792d7a6eb0f8
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-07-04 15:20:47 -0400

    Update cmd.go

--- vpn/client/cmd.go
+++ vpn/client/cmd.go
@@ -1,11 +1,27 @@
 package client
 
+import (
+	"log"
+	"net"
+	"strconv"
+	"vpn/node"
+)
+
 // figure out flag package and design skeleton version cli
 
 func StartCmd() {
-	// ip := GetIPAddr()
-	// port := NODE_PORT
-	// 
-	// req := FormRequest(ip, port)
-	// SendRequestToCoordServer(req)
+	IPAddr, port := GetClientAddr()
+	node := node.NewNode(IPAddr, port)
+	// form request to coord server
+	// Send request to coord server to get routetable
+
+}
+
+func GetClientAddr() (IP string, Port string) {
+	conn, err := net.Dial("udp", "8.8.8.8:80") // can use any address because it's udp
+	if err != nil {
+		log.Fatal(err)
+	}
+	clientAddr := conn.LocalAddr().(*net.UDPAddr)
+	return clientAddr.IP.String(), strconv.Itoa(clientAddr.Port)
 }
