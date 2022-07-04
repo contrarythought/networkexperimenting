88d015aaf6bdfc36da4d67d0a68ac20377919d8a
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-07-04 15:22:34 -0400

    Update node.go

--- vpn/node/node.go
+++ vpn/node/node.go
@@ -1,16 +1,14 @@
 package node
 
 type Node struct {
-	IP      string
-	Port    string
+	IP         string
+	Port       string
 	RouteTable map[string]bool
 }
 
-func NewNode(IP, Port string, RouteTable map[string]bool) *Node {
+func NewNode(IP, Port string) *Node {
 	return &Node{
-		IP: IP,
+		IP:   IP,
 		Port: Port,
-		RouteTable: RouteTable,
 	}
 }
-
